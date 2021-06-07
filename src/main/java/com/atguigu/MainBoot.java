4760c53410ad814eb70c41c54573dba92576334b
Lgy <Lgy@123.com>
2021-06-07 14:55:03 +0800

    第分支合并测试2提交git

--- src/main/java/com/atguigu/MainBoot.java
+++ src/main/java/com/atguigu/MainBoot.java
@@ -12,10 +12,10 @@ public class MainBoot {
         for (String name : names) {
             System.out.println(name);
         }
-
         boolean user = run.containsBean("user");
-        System.out.println("这个是hot-hix分支容器中Tom组件："+user);//false
-
+        System.out.println("容器中Tom组件："+user);//false
+        System.out.println("容器中Tom组件test："+user);//false
+        System.out.println("容器中Tom组件："+user);//false
         boolean pet = run.containsBean("pet");
         System.out.println("容器中user01组件："+pet);//true
 
